7d946b9dd39df61949c74c1c975637fcf9aeef75
anomumusical-eng <anomu.musical@gmail.com>
2026-04-10 13:25:49 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -6,16 +6,16 @@ import { StatusBar } from 'expo-status-bar';
 import { View, ActivityIndicator } from 'react-native';
 import { supabase } from './src/lib/supabase';
 import { Colors } from './src/lib/theme';
-
-import SplashScreen    from './src/screens/SplashScreen';
-import LoginScreen     from './src/screens/LoginScreen';
-import SignupScreen    from './src/screens/SignupScreen';
-import OnboardingScreen from './src/screens/OnboardingScreen';
-import HomeScreen      from './src/screens/HomeScreen';
-import CheckinScreen   from './src/screens/CheckinScreen';
-import LoopScreen      from './src/screens/LoopScreen';
-import HistoryScreen   from './src/screens/HistoryScreen';
-import ProfileScreen   from './src/screens/ProfileScreen';
+import SplashScreen       from './src/screens/SplashScreen';
+import LoginScreen        from './src/screens/LoginScreen';
+import SignupScreen       from './src/screens/SignupScreen';
+import SignupSuccessScreen from './src/screens/SignupSuccessScreen';
+import OnboardingScreen   from './src/screens/OnboardingScreen';
+import HomeScreen         from './src/screens/HomeScreen';
+import CheckinScreen      from './src/screens/CheckinScreen';
+import LoopScreen         from './src/screens/LoopScreen';
+import HistoryScreen      from './src/screens/HistoryScreen';
+import ProfileScreen      from './src/screens/ProfileScreen';
 
 const Stack = createStackNavigator();
 
@@ -48,9 +48,10 @@ export default function App() {
       <Stack.Navigator screenOptions={{ headerShown: false, cardStyle: { backgroundColor: Colors.bg } }}>
         {!session ? (
           <>
-            <Stack.Screen name="Splash"      component={SplashScreen} />
-            <Stack.Screen name="Login"       component={LoginScreen} />
-            <Stack.Screen name="Signup"      component={SignupScreen} />
+            <Stack.Screen name="Splash"         component={SplashScreen} />
+            <Stack.Screen name="Login"          component={LoginScreen} />
+            <Stack.Screen name="Signup"         component={SignupScreen} />
+            <Stack.Screen name="SignupSuccess"  component={SignupSuccessScreen} />
           </>
         ) : (
           <>
